22a203600232858fc310a288f2290f3a944b3471
aaranyak <aaranyak.ghosh@gmail.com>
2023-05-11 10:46:51 +0530

    Update

--- audio/audio.c
+++ audio/audio.c
@@ -2,27 +2,35 @@
 
 #include <alsa/asoundlib.h>
 
-int open_audio_device(snd_pcm_t *handle, /* Pcm device handle, remember to run "snd_pcm_close(handle)" at the end of program */
+int open_audio_device(snd_pcm_t **handle, /* Pcm device handle, remember to run "snd_pcm_close(handle)" at the end of program */
                       char *device_name, /* Use "default" for opening the default audio device */
                       int play_audio) /* Set to 1 if used for playing audio, otherwise set 0 */
 {
     /* Opens a ALSA pcm audio device */
-    snd_pcm_stream_t stream; /* Playback stream or capture stream */
-    int status; /* If negative, then error status. If 0, then success */
-    if (play_audio) stream = SND_PCM_STREAM_PLAYBACK; /* Set stream to playback stream */
-    else stream = SND_PCM_STREAM_CAPTURE; /* Set stream to capture */
-    status = snd_pcm_open(&handle, device_name, stream, 0); /* Open the pcm device */
-    if (status < 0) { /* If error occurs */
-        fprintf(stderr, "Error while opening pcm device \"%s\": %s.\n", device_name, snd_strerror(status)); /* Print error message */
-    }
+    int status = snd_pcm_open(handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
     return status;
 }
 
-int main(int argc, char *argv[]) {
-    /* Currently, do nothing */
-    snd_pcm_t *handle;
-    if (argc) {
-        int status = open_audio_device(handle, argv[1], 1);
+int set_hardware_params(snd_pcm_t *handle, /* Handle to audio device */
+                        snd_pcm_hw_params_t *hardware_params, /* Hardware params object */
+                        int resample_rate /* The resample rate of the track */ )
+{
+    /* Set the hardware parameters of an audio device */
+    int status = 0; /* Error status */
+    status = snd_pcm_hw_params_any(handle, hardware_params); /* Set hardware_params to default values*/
+    if (status < 0) { /* If error occurs */
+        fprintf(stderr, "Error while setting hardware params object: %s\n", snd_strerror(status)); /* Print error message */
+        return status; /* Return error status */
     }
     return status;
+}
+int main(int argc, char *argv[]) {
+    /* Currently, do nothing */ 
+    snd_pcm_t *handle;
+    snd_pcm_hw_params_t *hardware_params;
+    int status;
+    status = open_audio_device(&handle, "default", 1);
+    snd_pcm_hw_params_alloca(&hardware_params);
+    status = set_hardware_params(handle, hardware_params, 0);
+    return 1;
 }
